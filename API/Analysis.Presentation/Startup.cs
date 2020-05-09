905379864026629266871d2e065faab58e6d171e
Sn7a7ke <sn7a7ke@protonmail.com>
2020-05-09 21:49:23 +0300

    Added Swagger

--- API/Analysis.Presentation/Startup.cs
+++ API/Analysis.Presentation/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Analysis.Presentation
 {
@@ -32,11 +33,22 @@ namespace Analysis.Presentation
             services.AddSingleton<IStorage>(fileStorage);
             services.AddSingleton<IExternalStorage, PomCovidExternalStorage>();
             services.AddScoped<ICovidAppService, PomCovidAppService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Analysis", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Analysis API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
